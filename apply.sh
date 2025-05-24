e4c03c08de9aaa3a49c47c5187b0640db4de7647
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-05-24 20:46:56 -0230

    Remove cgroupv2 patches
    These break crDroid. I'll need to reach out to fakeman to see what they're for, but they don't seem to be essential so for now I'm going to go ahead and remove.

--- apply.sh
+++ apply.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd packages/modules/Connectivity
-echo "Applying patches to packages/modules/Connectivity"
-git am --signoff < ~/7420_patches-lineage-20.0/packages_modules_Connectivity/0004-Bring-back-traffic-indicators-for-legacy-devices.patch
-cd ../
 cd NetworkStack
 echo "Applying patches to NetworkStack"
 git am --signoff < ~/7420_patches-lineage-20.0/packages_modules_NetworkStack/0001-Revert-Enable-parsing-netlink-events-from-kernel-sin.patch
@@ -19,16 +15,11 @@ cd ../../
 cd frameworks/native
 echo "Applying patches to frameworks/native"
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0001-Disable-gpu-service.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0001-Revert-Remove-obsolete-debug-option.patch
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_native/0002-Add-back-pre-S-createEventQueue-function.patch
 cd ../../
 cd frameworks/base
 echo "Applying patches to frameworks/base"
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0001-Revert-fp-always-on-changes.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0001-Revert-CachedAppOptimizer-use-new-cgroup-api-for-fre.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0002-Revert-CachedAppOptimizer-remove-native-freezer-enab.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0003-Revert-CachedAppOptimizer-don-t-hardcode-freezer-pat.patch
-git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0004-CachedAppOptimizer-revert-freezer-to-cgroups-v1.patch
 cd ../../
 cd art
 echo "Applying patches to art"
@@ -38,11 +29,6 @@ cd ../
 cd external/perfetto
 echo "Applying patches to external/perfetto"
 git am --signoff < ~/7420_patches-lineage-20.0/external_perfetto/0001-perfetto-Conditionally-remove-version-check-for-memf.patch
-cd ../../
-cd system/core
-echo "Applying patches to system/core"
-git am --signoff < ~/7420_patches-lineage-20.0/system_core/0001-Fix-support-for-devices-without-cgroupv2-support.patch
-git am --signoff < ~/7420_patches-lineage-20.0/system_core/0002-Revert-libprocessgroup-switch-freezer-to-cgroup-v2.patch
 cd ../../
 cd hardware/lineage/interfaces
 echo "Applying patches to hardware/lineage/interfaces"
