ac72d3274bc5ed3c618aea18301a4433bef757dd
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-05-24 21:05:30 -0230

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd NetworkStack
+cd packages/modules/NetworkStack
 echo "Applying patches to NetworkStack"
 git am --signoff < ~/7420_patches-lineage-20.0/packages_modules_NetworkStack/0001-Revert-Enable-parsing-netlink-events-from-kernel-sin.patch
 cd ../
-cd adb
+cd packages/modules/adb
 echo "Applying patches to adb"
 git am --signoff < ~/7420_patches-lineage-20.0/packages_modules_adb/0001-adb-Bring-back-support-for-legacy-FunctionFS.patch
 cd ../../../
-cd security
+cd system/security
 echo "Applying patches to security"
 git am --signoff < ~/7420_patches-lineage-20.0/system_security/0001-keystore-hackup.patch
 cd ../../
