151d073f5e76b0edc8d2ed7968e48f432dd7f601
Evelyn Torres <lostentrepreneur439@yahoo.com>
2026-04-12 14:07:50 -0230

    Add hwui reset patch
    this patch was made for older adreno gpus, but we experience the page faults also it seems.
    
    although performance without the patch is a bit better, the jank in some scenarios is truly something to behold
    
    "OpenGLRenderer: Unable to match the desired swap behavior." is spammed in logs. Revisit this at a later date to maybe regain some performance

--- apply.sh
+++ apply.sh
@@ -16,6 +16,7 @@ cd ../../
 cd frameworks/base
 echo "Applying patches to frameworks/base"
 git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0001-Revert-fp-always-on-changes.patch
+git am --signoff < ~/7420_patches-lineage-20.0/frameworks_base/0002-hwui-reset-to-android-13.0.0_r13.patch
 cd ../../packages/apps/Nfc
 echo "Applying patches to Packages/apps/Nfc..."
 git am --signoff < ~/7420_patches-lineage-20.0/packages_apps_Nfc/Add_sysprop_to_prevent_FW_download.patch
